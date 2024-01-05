24899b11917b642f505171f7cdd790c8e83e7566
dotoolboy <dotoolboy@naver.com>
2024-01-05 11:14:09 +0900

    [➕ Add] Enemy_Animation
    - Animation 추가

--- Assets/Scripts/CHH/CHHScene.cs
+++ Assets/Scripts/CHH/CHHScene.cs
@@ -2,36 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CHHScene : MonoBehaviour
+public class CHHScene : BaseScene
 {
-    void Start()
+    public override void Clear()
     {
-        if (Main.ResourceManager.Loaded)
-        {
-            Main.DataManager.Initialize();
-        }
-        else
-        {
-            Main.ResourceManager.LoadAllAsync<UnityEngine.Object>("PreLoad", (key, count, totalCount) =>
-            {
-                Debug.Log($"[TestScene] Load asset {key} ({count} / {totalCount})");
-                if (count >= totalCount)
-                {
-                    Main.DataManager.Initialize();
-                    //Main.Game.Initialize();
-                    InitializeGame();
-                }
-            });
-        }
     }
 
-    private void InitializeGame()
+    protected override bool Initialize()
     {
-        
+        if (!base.Initialize()) return false;
 
-      
         Main.ObjectManager.Spawn<Enemy>("Blue", new(7, 2));
         Main.ObjectManager.Spawn<Enemy>("Black", new(7, 1));
-        
+
+        return true;
     }
 }
--- Assets/Scripts/Scene/BaseScene.cs
+++ Assets/Scripts/Scene/BaseScene.cs
@@ -14,7 +14,7 @@ public abstract class BaseScene : MonoBehaviour
         if (Main.ResourceManager.Loaded)
         {
             
-            //Main.Data.Initialize();
+            Main.DataManager.Initialize();
             //Main.Game.Initialize();
             Initialize();
         }
@@ -25,7 +25,7 @@ public abstract class BaseScene : MonoBehaviour
                 if (count >= totalCount)
                 {
                     Main.ResourceManager.Loaded = true;
-                    //Main.Data.Initialize();
+                    Main.DataManager.Initialize();
                     //Main.Game.Initialize();
                     Initialize();
                 }
