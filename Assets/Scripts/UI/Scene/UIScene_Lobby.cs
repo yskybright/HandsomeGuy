a193add27da43a1d0b2184abcf4147fa0489347f
정채운 <toyboy01113@gmail.com>
2024-01-07 03:18:51 +0900

    [💫 Feature] Character
    로비, 게임 캐릭터 변경, 어드레서블 스프라이트 추가

--- Assets/Scripts/UI/Scene/UIScene_Lobby.cs
+++ Assets/Scripts/UI/Scene/UIScene_Lobby.cs
@@ -164,8 +164,13 @@ public class UIScene_Lobby : UIScene, IChatable
     {
         var tmp = Main.ResourceManager.Instantiate("User.prefab", _userPos);        
         User newItem = tmp.GetOrAddComponent<User>();
+        
         if (participant.IsSelf)
+        {
             _me = newItem;
+            _me.SetImage();
+        }
+
         Users.Add(newItem);
 
         participant.SetLocalVolume(0);
